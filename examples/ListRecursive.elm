542ca26993b24d2d09c108ff28eabc73d78ce236
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-02-27 17:24:51 -0500

    Add example of listing files and directories

---
+++ examples/ListRecursive.elm
@@ -0,0 +1,57 @@
+port module Main exposing (..)
+
+import Json.Encode exposing (Value)
+import Kintail.Script as Script exposing (Script, FileError)
+
+
+listRecursively : Int -> String -> Script FileError ()
+listRecursively level directory =
+    let
+        indentation =
+            String.repeat level "    "
+    in
+        Script.do
+            [ Script.listSubdirectories directory
+                |> Script.andThen
+                    (Script.forEach
+                        (\subdirectory ->
+                            Script.do
+                                [ Script.print
+                                    (indentation ++ subdirectory ++ "/")
+                                , listRecursively (level + 1)
+                                    (directory ++ "/" ++ subdirectory)
+                                ]
+                        )
+                    )
+            , Script.listFiles directory
+                |> Script.andThen
+                    (Script.forEach
+                        (\filename -> Script.print (indentation ++ filename))
+                    )
+            ]
+
+
+script : List String -> Script Int ()
+script arguments =
+    case arguments of
+        [ directory ] ->
+            listRecursively 0 directory
+                |> Script.onError (.message >> handleError)
+
+        _ ->
+            Script.print "Please supply one directory name"
+
+
+handleError : String -> Script Int ()
+handleError message =
+    Script.print ("ERROR: " ++ message) |> Script.andThen (\() -> Script.fail 1)
+
+
+port requestPort : Value -> Cmd msg
+
+
+port responsePort : (Value -> msg) -> Sub msg
+
+
+main =
+    Script.program script requestPort responsePort
