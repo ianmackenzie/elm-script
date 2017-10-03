f9efc452c7f703b5886fed0d63ee32783a56ffb0
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-10-03 18:18:48 -0400

    Switch to prettier for code formatting

--- runners/node/cli.js
+++ runners/node/cli.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-'use strict'
+"use strict";
 
-let run = require('./lib.js')
+let run = require("./lib.js");
 
 if (process.argv.length >= 3) {
-  let elmFileName = process.argv[2]
-  run(elmFileName, process.argv.slice(3))
+  let elmFileName = process.argv[2];
+  run(elmFileName, process.argv.slice(3));
 } else {
-  console.log("Run as 'elm-run Script.elm [arguments]'")
+  console.log("Run as 'elm-run Script.elm [arguments]'");
 }
--- runners/node/lib.js
+++ runners/node/lib.js
@@ -1,128 +1,151 @@
-'use strict'
+"use strict";
 
-let majorVersion = 1
-let minorVersion = 0
+let majorVersion = 1;
+let minorVersion = 0;
 
-let vm = require('vm')
-let fs = require('fs')
-let path = require('path')
-let child_process = require('child_process')
-let compileToString = require('node-elm-compiler').compileToString
+let vm = require("vm");
+let fs = require("fs");
+let path = require("path");
+let child_process = require("child_process");
+let compileToString = require("node-elm-compiler").compileToString;
 
-function listEntities (request, responsePort, statsPredicate) {
+function listEntities(request, responsePort, statsPredicate) {
   try {
-    let directory = request.value
-    let results = fs.readdirSync(directory).filter(function (entity) {
-      return statsPredicate(fs.statSync(path.resolve(directory, entity)))
-    })
-    responsePort.send(results)
+    let directory = request.value;
+    let results = fs.readdirSync(directory).filter(function(entity) {
+      return statsPredicate(fs.statSync(path.resolve(directory, entity)));
+    });
+    responsePort.send(results);
   } catch (error) {
-    responsePort.send({code: error.code, message: error.message})
+    responsePort.send({ code: error.code, message: error.message });
   }
 }
 
 function runCompiledJs(compiledJs, commandLineArgs) {
   // Set up browser-like context in which to run compiled Elm code
-  global.XMLHttpRequest = require('xhr2')
-  global.setTimeout = require('timers').setTimeout
+  global.XMLHttpRequest = require("xhr2");
+  global.setTimeout = require("timers").setTimeout;
   // Run Elm code to create the 'Elm' object
-  vm.runInThisContext(compiledJs)
+  vm.runInThisContext(compiledJs);
 
   // Create Elm worker and get its request/response ports
-  let script = global['Elm'].Main.worker(commandLineArgs)
-  let requestPort = script.ports.requestPort
-  let responsePort = script.ports.responsePort
+  let script = global["Elm"].Main.worker(commandLineArgs);
+  let requestPort = script.ports.requestPort;
+  let responsePort = script.ports.responsePort;
 
   // Listen for requests, send responses when required
-  requestPort.subscribe(function (request) {
+  requestPort.subscribe(function(request) {
     switch (request.name) {
-      case 'requiredVersion':
-        let requiredMajorVersion = request.value[0]
-        let requiredMinorVersion = request.value[1]
-        let describeCurrentVersion = ' (current elm-run version: ' + majorVersion + '.' + minorVersion + ')'
+      case "requiredVersion":
+        let requiredMajorVersion = request.value[0];
+        let requiredMinorVersion = request.value[1];
+        let describeCurrentVersion =
+          " (current elm-run version: " +
+          majorVersion +
+          "." +
+          minorVersion +
+          ")";
         if (requiredMajorVersion !== majorVersion) {
-          console.log('Version mismatch: script requires elm-run major version ' + requiredMajorVersion + describeCurrentVersion)
+          console.log(
+            "Version mismatch: script requires elm-run major version " +
+              requiredMajorVersion +
+              describeCurrentVersion
+          );
           if (requiredMajorVersion > majorVersion) {
-            console.log('Please update to a newer version of elm-run')
+            console.log("Please update to a newer version of elm-run");
           } else {
-            console.log('Please update script to use a newer version of the kintail/script package')
+            console.log(
+              "Please update script to use a newer version of the kintail/script package"
+            );
           }
-          process.exit(1)
+          process.exit(1);
         } else if (requiredMinorVersion > minorVersion) {
-          let requiredVersionString = requiredMajorVersion + '.' + requiredMinorVersion
-          console.log('Version mismatch: script requires elm-run version at least ' + requiredVersionString + describeCurrentVersion)
-          console.log('Please update to a newer version of elm-run')
-          process.exit(1)
+          let requiredVersionString =
+            requiredMajorVersion + "." + requiredMinorVersion;
+          console.log(
+            "Version mismatch: script requires elm-run version at least " +
+              requiredVersionString +
+              describeCurrentVersion
+          );
+          console.log("Please update to a newer version of elm-run");
+          process.exit(1);
         } else {
-          responsePort.send(null)
+          responsePort.send(null);
         }
-        break
-      case 'print':
-        console.log(request.value)
-        responsePort.send(null)
-        break
-      case 'exit':
-        process.exit(request.value)
-      case 'readFile':
+        break;
+      case "print":
+        console.log(request.value);
+        responsePort.send(null);
+        break;
+      case "exit":
+        process.exit(request.value);
+      case "readFile":
         try {
-          let filename = request.value
-          let contents = fs.readFileSync(filename, 'utf8')
-          responsePort.send(contents)
+          let filename = request.value;
+          let contents = fs.readFileSync(filename, "utf8");
+          responsePort.send(contents);
         } catch (error) {
-          responsePort.send({code: error.code, message: error.message})
+          responsePort.send({ code: error.code, message: error.message });
         }
-        break
-      case 'writeFile':
+        break;
+      case "writeFile":
         try {
-          let filename = request.value.filename
-          let contents = request.value.contents
-          fs.writeFileSync(filename, contents, 'utf8')
-          responsePort.send(null)
+          let filename = request.value.filename;
+          let contents = request.value.contents;
+          fs.writeFileSync(filename, contents, "utf8");
+          responsePort.send(null);
         } catch (error) {
-          responsePort.send({code: error.code, message: error.message})
+          responsePort.send({ code: error.code, message: error.message });
         }
-        break
-      case 'getEnvironmentVariable':
-        responsePort.send(process.env[request.value] || null)
-        break
-      case 'listFiles':
-        listEntities(request, responsePort, stats => stats.isFile())
-        break
-      case 'listSubdirectories':
-        listEntities(request, responsePort, stats => stats.isDirectory())
-        break
-      case 'execute':
+        break;
+      case "getEnvironmentVariable":
+        responsePort.send(process.env[request.value] || null);
+        break;
+      case "listFiles":
+        listEntities(request, responsePort, stats => stats.isFile());
+        break;
+      case "listSubdirectories":
+        listEntities(request, responsePort, stats => stats.isDirectory());
+        break;
+      case "execute":
         try {
-          let command = request.value.command
-          let args = request.value.arguments
-          let options = {encoding: 'utf8', maxBuffer: 1024 * 1024 * 1024}
-          let output = child_process.execSync(command + ' ' + args.join(' '), options)
-          responsePort.send(output)
+          let command = request.value.command;
+          let args = request.value.arguments;
+          let options = { encoding: "utf8", maxBuffer: 1024 * 1024 * 1024 };
+          let output = child_process.execSync(
+            command + " " + args.join(" "),
+            options
+          );
+          responsePort.send(output);
         } catch (error) {
           if (error.status !== null) {
-            responsePort.send({error: 'exited', code: error.status})
+            responsePort.send({ error: "exited", code: error.status });
           } else if (error.signal !== null) {
-            responsePort.send({error: 'terminated'})
+            responsePort.send({ error: "terminated" });
           } else {
-            responsePort.send({error: 'failed', message: error.message})
+            responsePort.send({ error: "failed", message: error.message });
           }
         }
-        break
+        break;
       default:
-        console.log('Internal error - unexpected request: ' + request)
-        console.log('Try updating to newer versions of elm-run and the kintail/script package')
-        process.exit(1)
+        console.log("Internal error - unexpected request: " + request);
+        console.log(
+          "Try updating to newer versions of elm-run and the kintail/script package"
+        );
+        process.exit(1);
     }
-  })
+  });
 }
 
-module.exports = function (elmFileName, commandLineArgs) {
-  let absolutePath = path.resolve(elmFileName)
-  let directory = path.dirname(absolutePath)
-  let compileOptions = {yes: true, cwd: directory}
-  compileToString(absolutePath, compileOptions).then(function (compiledJs) {
-    runCompiledJs(compiledJs, commandLineArgs)
-  }).catch(function (error) {
-    console.log(error.message)
-  })
-}
+module.exports = function(elmFileName, commandLineArgs) {
+  let absolutePath = path.resolve(elmFileName);
+  let directory = path.dirname(absolutePath);
+  let compileOptions = { yes: true, cwd: directory };
+  compileToString(absolutePath, compileOptions)
+    .then(function(compiledJs) {
+      runCompiledJs(compiledJs, commandLineArgs);
+    })
+    .catch(function(error) {
+      console.log(error.message);
+    });
+};
