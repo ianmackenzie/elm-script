7ef210a673195895cc5499256cbee20fdc3c5eff
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-10-16 16:28:34 -0400

    Add initial PowerShell example

---
+++ examples/PowerShellTest.elm
@@ -0,0 +1,45 @@
+port module Main exposing (..)
+
+import Json.Encode exposing (Value)
+import Kintail.Script as Script exposing (Context, Script)
+import Kintail.Script.Shell as Shell exposing (ProcessError(..))
+
+
+script : Context -> Script Int ()
+script { shell } =
+    Shell.execute "PowerShell -Command Get-ChildItem -Name -Path *.elm" shell
+        |> Script.map String.lines
+        |> Script.map (List.map String.trim)
+        |> Script.map (List.filter (not << String.isEmpty))
+        |> Script.andThen (Script.forEach (String.toUpper >> Script.print))
+        |> Script.onError (toErrorString >> handleError)
+
+
+toErrorString : ProcessError -> String
+toErrorString processError =
+    case processError of
+        ProcessFailed message ->
+            message
+
+        ProcessWasTerminated ->
+            "Process was terminated"
+
+        ProcessExitedWithError code ->
+            "Process exited with code " ++ toString code
+
+
+handleError : String -> Script Int a
+handleError message =
+    Script.print ("ERROR: " ++ message)
+        |> Script.andThen (\() -> Script.fail 1)
+
+
+port requestPort : Value -> Cmd msg
+
+
+port responsePort : (Value -> msg) -> Sub msg
+
+
+main : Script.Program
+main =
+    Script.program script requestPort responsePort
