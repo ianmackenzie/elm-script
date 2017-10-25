d17fed2304f70baff07b60b46a94f38156eec2d7
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-10-25 14:51:47 -0400

    Add testing script
    Script that runs other scripts and checks for expected output

---
+++ examples/Tests.elm
@@ -0,0 +1,76 @@
+port module Main exposing (..)
+
+import Json.Encode exposing (Value)
+import Script exposing (Script)
+import Script.Shell as Shell exposing (Shell)
+
+
+test : Shell -> List ( String, String ) -> Script Int ()
+test shell testCases =
+    testCases
+        |> Script.forEach
+            (\( command, expectedOutput ) ->
+                Shell.execute command shell
+                    |> Script.onError
+                        (\processError ->
+                            Script.print ("Running '" ++ command ++ "' failed")
+                                |> Script.andThen (\() -> Script.fail 1)
+                        )
+                    |> Script.andThen
+                        (\output ->
+                            if String.trim output == expectedOutput then
+                                Script.print ("PASSED: " ++ command)
+                            else
+                                Script.print
+                                    ("FAILED: "
+                                        ++ command
+                                        ++ "\n\n"
+                                        ++ "Expected output:\n\n"
+                                        ++ expectedOutput
+                                        ++ "\n\nActual output:\n\n"
+                                        ++ String.trim output
+                                        ++ "\n"
+                                    )
+                                    |> Script.andThen (\() -> Script.fail 1)
+                        )
+            )
+        |> Script.andThen
+            (\() ->
+                Script.print <|
+                    "Success! "
+                        ++ toString (List.length testCases)
+                        ++ " tests passed"
+            )
+
+
+script : Script.Context -> Script Int ()
+script { shell } =
+    test shell
+        [ ( "elm-run ForEach.elm 1 2 undefined 3.5"
+          , "1 squared is 1\n2 squared is 4\nundefined is not a number!\n3.5 squared is 12.25"
+          )
+        , ( "elm-run GetElmVersion.elm"
+          , "Current Elm version: 0.18.0"
+          )
+        , ( "elm-run HelloWorld.elm"
+          , "Hello World!"
+          )
+        , ( "elm-run LineCounts.elm test.txt"
+          , "test.txt: 3 lines"
+          )
+        ]
+
+
+
+-- Boilerplate
+
+
+port requestPort : Value -> Cmd msg
+
+
+port responsePort : (Value -> msg) -> Sub msg
+
+
+main : Script.Program
+main =
+    Script.program script requestPort responsePort
