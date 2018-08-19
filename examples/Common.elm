d415ae6bc148cee2a1ef381609b33a2c608d0360
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-08-18 21:25:56 -0400

    Add Common module to reduce boilerplate in examples

---
+++ examples/Common.elm
@@ -0,0 +1,16 @@
+port module Common exposing (..)
+
+import Json.Encode exposing (Value)
+import Script exposing (Script)
+
+
+handleError : (x -> String) -> x -> Script Int a
+handleError toMessage error =
+    Script.printLine ("ERROR: " ++ toMessage error)
+        |> Script.andThen (\() -> Script.fail 1)
+
+
+port requestPort : Value -> Cmd msg
+
+
+port responsePort : (Value -> msg) -> Sub msg
