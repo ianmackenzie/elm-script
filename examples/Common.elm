5514aa3d9f8296bd4997bc81477d1eded8b307f9
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2018-08-19 20:10:00 -0400

    Tweak error reporting format in examples
    Make it more obvious that errors are being caught properly and not just printed out by Node

--- examples/Common.elm
+++ examples/Common.elm
@@ -6,7 +6,7 @@ import Script exposing (Script)
 
 handleError : (x -> String) -> x -> Script Int a
 handleError toMessage error =
-    Script.printLine ("ERROR: " ++ toMessage error)
+    Script.printLine ("- SCRIPT ERROR - " ++ toMessage error)
         |> Script.andThen (\() -> Script.fail 1)
 
 
