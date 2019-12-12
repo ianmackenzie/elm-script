37ce69be960411f4f2cb84724cb106ae44ae2eba
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2019-12-11 20:24:04 -0500

    Add script to test that all examples compile OK =)

---
+++ examples/TestCompilation.elm
@@ -0,0 +1,33 @@
+module TestCompilation exposing (main)
+
+import Example exposing (handleError)
+import Script exposing (Script)
+import Script.Directory as Directory
+import Script.File as File
+
+
+script : Script.Context -> Script Int ()
+script { shell, workingDirectory } =
+    Directory.listFiles workingDirectory
+        |> Script.onError (handleError .message)
+        |> Script.andThen
+            (Script.forEach
+                (\file ->
+                    if File.name file |> String.endsWith ".elm" then
+                        Script.do
+                            [ Script.printLine ("Compiling " ++ File.name file)
+                            , shell.execute "elm"
+                                [ "make", "--output", "/dev/null", File.path file ]
+                                |> Script.ignore
+                                |> Script.onError (\error -> Script.fail 1)
+                            ]
+
+                    else
+                        Script.do []
+                )
+            )
+
+
+main : Script.Program
+main =
+    Example.program script
