97a224588a833e2ff97b34e13d59a286429af7e6
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2019-12-13 13:14:13 -0500

    Add example script that checks for uncommitted Git changes

---
+++ examples/CheckGitStatus.elm
@@ -0,0 +1,61 @@
+module CheckGitStatus exposing (main)
+
+import Example
+import Script exposing (Script, UserPrivileges)
+import Script.Directory as Directory exposing (Directory, Writable)
+
+
+checkDirectory : Directory Writable -> UserPrivileges -> Script Int ()
+checkDirectory directory userPrivileges =
+    Script.printLine ("Checking " ++ Directory.name directory)
+        |> Script.followedBy
+            (Script.executeWith userPrivileges
+                { command = "git"
+                , arguments = [ "status" ]
+                , workingDirectory = directory
+                }
+                |> Script.andThen
+                    (\output ->
+                        if String.contains "nothing to commit, working tree clean" output then
+                            Script.do []
+
+                        else
+                            Script.printLine output
+                    )
+                |> Script.onError
+                    (\error ->
+                        Script.printLine "Running Git failed"
+                            |> Script.followedBy (Script.fail 1)
+                    )
+            )
+
+
+script : Script.Init -> Script Int ()
+script { userPrivileges } =
+    let
+        rootDirectory =
+            Directory.writable userPrivileges "C:/Git/ianmackenzie"
+    in
+    Directory.listSubdirectories rootDirectory
+        |> Script.onError (Example.handleError .message)
+        |> Script.andThen
+            (Script.forEach
+                (\directory ->
+                    Directory.checkExistence (Directory.subdirectory ".git" directory)
+                        |> Script.onError (Example.handleError .message)
+                        |> Script.andThen
+                            (\existence ->
+                                case existence of
+                                    Directory.Exists ->
+                                        checkDirectory directory userPrivileges
+
+                                    _ ->
+                                        Script.do []
+                            )
+                )
+            )
+
+
+main : Script.Program
+main =
+    Example.program script
