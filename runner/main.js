cc0fb47dda8b2787baee76410b3a0fbbce867944
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2020-10-06 21:30:44 -0400

    Update to recent version of Deno stdlib
    Avoid type-checking error

--- runner/main.js
+++ runner/main.js
@@ -7,7 +7,7 @@ const minorVersion = 2;
 const majorProtocolVersion = 9;
 const minorProtocolVersion = 1;
 
-import * as path from "https://deno.land/std@0.56.0/path/mod.ts";
+import * as path from "https://deno.land/std@0.71.0/path/mod.ts";
 
 const tempDirectoriesToRemove = [];
 
